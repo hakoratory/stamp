f681b6d70c30f7c9a4a06d53dc951ebca1698de5
はこだてたろう <hakodate.1106v@gmail.com>
2021-01-06 22:20:22 +0900

    Fixed image loading process

--- webpack.config.js
+++ webpack.config.js
@@ -32,8 +32,10 @@ module.exports = {
           ]
        },
        {
-         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-         type: 'asset/resource'
+         test: /\.(png|svg|jpg|jpeg|gif)$/,
+         use: {
+           loader: 'url-loader',
+         }
        }
     ],
  },
