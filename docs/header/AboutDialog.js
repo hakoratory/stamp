83539bf5c0c5b3ac7cf3d93269569baa1e13fcec
はこだてたろう <hakodate.1106v@gmail.com>
2021-01-16 10:15:49 +0900

    update minor-version

--- docs/header/AboutDialog.js
+++ docs/header/AboutDialog.js
@@ -25,7 +25,7 @@ function AboutDialog(props){
                 <br/>
                 Version
                 <br/>
-                1.2
+                1.3
                 <br/>
                 <br/>
                 How to stamp
