ab1f53281eb292d3dc72bc9e38fbab34fa2c1129
はこだてたろう <hakodate.1106v@gmail.com>
2021-02-05 16:25:24 +0900

    fixed font of SwipeableSwitch

--- docs/components/stamp/SwipeableSwitch.js
+++ docs/components/stamp/SwipeableSwitch.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import SwipeableViews from 'react-swipeable-views'
 import { makeStyles } from '@material-ui/core/styles'
-import { Box } from '@material-ui/core'
+import { Box, Typography } from '@material-ui/core'
 
 const useStyles = makeStyles({
     root: {
@@ -15,62 +15,72 @@ const useStyles = makeStyles({
         userSelect: "none",
     },
     canvas_left: {
-        textAlign: "center",
-        fontWeight: "bold",
         color: "#4D4D4D",
         //borderBottom: "30px solid #FFFFFF",
         //borderRight: "15px solid transparent",
         height: "0",
         width: "80%",
-        lineHeight: "30px",
     },
     canvas_right: {
-        textAlign: "center",
-        fontWeight: "bold",
         color: "#DDDDDD",
         borderBottom: "30px solid #C0C0C0",
         borderLeft: "15px solid transparent",
         height: "0",
         width: "20%",
-        lineHeight: "30px",
         fontSize: "0.8em",
         
     },
     palette_left: {
-        textAlign: "center",
-        fontWeight: "bold",
         color: "#DDDDDD",
         borderBottom: "30px solid #C0C0C0",
         borderRight: "15px solid transparent",
         height: "0",
         width: "20%",
-        lineHeight: "30px",
         fontSize: "0.8em",
     },
     palette_right: {
-        textAlign: "center",
-        fontWeight: "bold",
         color: "#4D4D4D",
         //borderBottom: "30px solid #FFFFFF",
         //borderLeft: "15px solid transparent",
         height: "0",
         width: "80%",
-        lineHeight: "30px",
     }
 })
+
+const StyledBox = (props) => {
+    return <Box className={props.className} lineHeight="30px" fontWeight="fontWeightBold" textAlign="center">{props.children}</Box>
+}
 
 export default function SwipeableSwitch(props){
     const classes = useStyles()
     return (
         <SwipeableViews onChangeIndex={props.onChangeIndex} enableMouseEvents>
             <Box className={classes.root}>
-                <Box display="flex" flexDirection="row">
-                    <Box className={classes.canvas_left}>Canvas</Box><Box className={classes.canvas_right}>Palette</Box>
+                <Box display="flex" flexDirection="row" height="30px">
+                    <Typography component="div" className={classes.canvas_left} gutterBottom>
+                        <StyledBox>
+                            Canvas
+                        </StyledBox>
+                    </Typography>
+                    <Typography component="div" className={classes.canvas_right} gutterBottom>
+                        <StyledBox>
+                            Palette
+                        </StyledBox>
+                    </Typography>
                 </Box>
             </Box>
             <Box className={classes.root}>
-                <Box display="flex" flexDirection="row">
-                    <Box className={classes.palette_left}>Canvas</Box><Box className={classes.palette_right}>Palette</Box>
+                <Box display="flex" flexDirection="row" height="30px">
+                    <Typography component="div" className={classes.palette_left} gutterBottom>
+                        <StyledBox>
+                            Canvas
+                        </StyledBox>
+                    </Typography>
+                    <Typography component="div" className={classes.palette_right} gutterBottom>
+                        <StyledBox>
+                            Palette
+                        </StyledBox>
+                    </Typography>
                 </Box>
             </Box>
         </SwipeableViews>
